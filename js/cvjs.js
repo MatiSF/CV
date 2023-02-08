f86a37b171965c2ec632c4f3e170fd9d2b18787d
MatiSF <msf4143@gmail.com>
2023-02-08 01:08:44 -0300

    agregue funciones js en botones, peticione json aun sin poder utilizarlo

--- js/cvjs.js
+++ js/cvjs.js
@@ -1,1 +1,13 @@
-console.log ("hola mundo")
+function boton() {
+    alert("hola")
+};
+
+function asd() {
+    alert('Preparece para lo que esta a punto de ver!')
+    document.getElementById("disfrute").innerHTML = "Espero lo haya disfrutado" ;
+};
+
+fetch('https://randomuser.me/api/')
+    .then(res=>res.json())
+    .then(res=>console.log(res.results))
+
